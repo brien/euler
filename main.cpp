52a570fc3863194d4ec9dcf0265df394c628636a
Brien Smith-Martinez <bsmartinez@gmail.com>
2013-03-04 16:26:38 -0600

    First commit. Problem 1

---
+++ main.cpp
@@ -0,0 +1,38 @@
+//  A program to demonstrate solutions to the Project Euler problems.
+//  (projecteuler.net)
+//
+//  Copyright 2013 Brien Smith-Martinez
+//
+/*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 2 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sum = 0;
+	for(int i = 0; i < 10; i++)
+	{
+		if(i%3==0)
+		{
+			sum+=i;
+		}
+		else if (i%5==0)
+		{
+			sum+=i;
+		}
+	}
+	cout << sum << endl;
+}
