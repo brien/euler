18f7da74fd640f0e03efcfa601ad0f671434ebb7
Brien Smith Martinez <bsmartinez@gmail.com>
2015-05-03 11:54:10 +0200

    Problem 5: improved brute force

--- main.cpp
+++ main.cpp
@@ -147,15 +147,16 @@ int four()
 
 long long int five()
 {
-	long long int num = 2520;
-	for( int i = 11; i < 21; i++)
-	{
-		num *= i;
-	}
-	while (num > 1)
+	long long int num = 20;
+	while ( num %  2 != 0 || num %  3 != 0 || num %  4 != 0 || num %  5 != 0 ||
+       		num %  6 != 0 || num %  7 != 0 || num %  8 != 0 || num %  9 != 0 ||
+         	num % 10 != 0 || num % 11 != 0 || num % 12 != 0 || num % 13 != 0 ||
+        	num % 14 != 0 || num % 15 != 0 || num % 16 != 0 || num % 17 != 0 ||
+         	num % 18 != 0 || num % 19 != 0 || num % 20 != 0)
 	{
 		cout << num << endl;
-		num--;
+		num+=20;
 	}	
+
 	return num;
 }
